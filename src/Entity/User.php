2e67a10ea2762355585ea29cb29b7a4f0f81a3f8
Mateusz Mierzyński <mateusz.mateusz992@gmail.com>
2020-11-28 16:51:58 +0100

    Created simple authentication

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -40,7 +40,17 @@ class User implements UserInterface
     /**
      * @ORM\OneToMany(targetEntity=Address::class, mappedBy="owner", orphanRemoval=true)
      */
-    private $adresses;
+    private $addresses;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $lastName;
 
     public function __construct()
     {
@@ -128,29 +138,53 @@ class User implements UserInterface
     /**
      * @return Collection|Address[]
      */
-    public function getAdresses(): Collection
+    public function getAddresses(): Collection
     {
-        return $this->adresses;
+        return $this->addresses;
     }
 
-    public function addAdress(Address $adress): self
+    public function addAddress(Address $address): self
     {
-        if (!$this->adresses->contains($adress)) {
-            $this->adresses[] = $adress;
-            $adress->setOwner($this);
+        if (!$this->addresses->contains($address)) {
+            $this->addresses[] = $address;
+            $address->setOwner($this);
         }
 
         return $this;
     }
 
-    public function removeAdress(Address $adress): self
+    public function removeAddress(Address $address): self
     {
-        if ($this->adresses->removeElement($adress)) {
+        if ($this->addresses->removeElement($address)) {
             // set the owning side to null (unless already changed)
-            if ($adress->getOwner() === $this) {
-                $adress->setOwner(null);
+            if ($address->getOwner() === $this) {
+                $address->setOwner(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
 
         return $this;
     }
